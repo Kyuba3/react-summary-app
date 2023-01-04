27aa26790ded5070aa4ec5de5d61d7a6f7a4b3f8
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-01-04 15:37:28 +0100

    change task in package.json and modify RenderALLTables

--- react-summary-app/src/components/features/RenderAllTables.js
+++ react-summary-app/src/components/features/RenderAllTables.js
@@ -11,12 +11,12 @@ const RenderAllTables = () => {
 
     return(
         <Container>
-          <Row className="d-flex flex-row col-12">
+          <Row className="d-flex flex-row col-12 pt-3">
             {tables.map(table =>(
               <Card key={table.id} className="d-inline-flex flex-row justify-content-center pt-5">
-                <Card.Title className="col-2"><b>Table </b>{table.id}</Card.Title>
-                <Card.Text className="col-7"><b>Status: </b>{table.status}</Card.Text>
-                <Nav className="">
+                <Card.Title className="col-2 fs-3"><b>Table </b>{table.id}</Card.Title>
+                <Card.Text className="col-8 fs-5"><b>Status: </b>{table.status}</Card.Text>
+                <Nav className="pb-1">
                     <Nav.Link as={NavLink} to={"/table/" + table.id}>
                         <Button variant="primary" className="col-12 mb-3">
                             Show More
